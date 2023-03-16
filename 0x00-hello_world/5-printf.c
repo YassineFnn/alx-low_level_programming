b13234eda59116365b101bf7d176efc5ead5bd35
YassineFnn <founounou2013@gmail.com>
2023-03-16 05:53:22 -0700

    newcommit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 /**
- *dummy function
+ *main - Entry point
+ *
+ *Description: output a sentence
+ *
+ *rReturn: always 0
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
