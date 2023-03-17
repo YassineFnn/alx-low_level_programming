ede6e7662930dff2dd9503f37ea64e49e12892f2
YassineFnn <founounou2013@gmail.com>
2023-03-17 13:39:22 -0700

    newcommit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,13 +5,13 @@
  *
  *Description: prints alphabets in reverse order
  *
- *Return : always 0
+ *Return: always 0
  */
 
 int main(void)
 {
 int i;
-for (i = 96 + 25; i >= 96; i--)
+for (i = 97 + 25; i >= 96; i--)
 {
 putchar(i);
 }
