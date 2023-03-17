9c493bdecf1a6a8ed2b413c9b74d60c51f341fcd
YassineFnn <founounou2013@gmail.com>
2023-03-17 13:18:16 -0700

    newcommit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Description: priting numbers
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+int i;
+
+for(i = 0; i < 10; ++i)
+printf("%d", i);
+
+return (0);
+}
