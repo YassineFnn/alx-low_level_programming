701dce894cd42b7ddd0d5912c0c1b61e0ec6b684
YassineFnn <founounou2013@gmail.com>
2023-03-17 12:54:57 -0700

    newcommit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Description: prints the alphabet
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+for(int i = 97; i < 97 + 26; ++i){
+putchar(i);
+}
+return (0);
+}
