594e38af1ceedbd1511ea6e0376c24222c631b09
YassineFnn <founounou2013@gmail.com>
2023-03-17 13:05:55 -0700

    newcommit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**                                                                                                                                                           
- *main - Entry point of the program                                                                                                                           
- *                                                                                                                                                            
- *Description: prints the alphabet                                                                                                                            
- *                                                                                                                                                            
- *Return: always 0                                                                                                                                            
+ *main - Entry point of the program 
+ *                                 
+ *Description: prints the alphabet
+ *                               
+ *Return: always 0
  */
 
 int main(void)
