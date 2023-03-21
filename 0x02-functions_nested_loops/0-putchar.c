4763e4fdd9b000c5fd563ffc84a45c3be9eb1d9a
YassineFnn <founounou2013@gmail.com>
2023-03-21 14:19:57 -0700

    newcommit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,13 +13,13 @@ char c[] = "_putchar\n";
 int i;
 for (i = 0; c[i] != '\0'; i++)
 {
-_putchar(c[i]);
+putch(c[i]);
 }
 return (0);
 }
 
 /**
- * _putchar - writes the character c to stdout
+ * putch - writes the character c to stdout
  *
  * Description: The character c to write int stdout
  * @c: the character to be printed
@@ -27,7 +27,7 @@ return (0);
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int putch(char c)
 {
 return (write(1, &c, 1));
 }
