eec77dd6a594421c5de525af1210ddaa5e06ab74
YassineFnn <founounou2013@gmail.com>
2023-04-20 15:26:41 -0700

    newcommit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,6 +8,7 @@
  * @action: pointer to function
  * Return: void
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 int i;
@@ -18,4 +19,5 @@ return;
 for (i = 0; i < (int) size; i++)
 {
 action(array[i]);
+}
 }
